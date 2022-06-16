f060b8ba8babdd9b211895cf8860ebff35e4289c
Ananta Kr. Roy <kr.innovate@gmail.com>
2022-06-16 14:13:20 +0530

    Rotate image module

---
+++ src/utils/rotate.ts
@@ -0,0 +1,13 @@
+import sharp from "sharp";
+import Info from "../interface/info";
+
+const dir = `thumbs`;
+
+const rotate = async (filename: string, angle: number): Promise<Info> => {
+  const promise = await sharp("assets/" + filename)
+    .rotate(angle)
+    .toFile(`${dir}/rotated_${angle}_${filename}`);
+  return promise;
+};
+
+export default rotate;
